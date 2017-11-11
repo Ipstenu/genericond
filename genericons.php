233e3f8e195bf2e98e5a7e010172938b2c98c703
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2017-11-10 17:27:41 -0800

    4.9 compat

--- genericons.php
+++ genericons.php
@@ -513,7 +513,7 @@ class GenericonsHELF {
 	 */
     public function plugin_links($links, $file) {
         if ($file == plugin_basename(__FILE__)) {
-                $donate_link = '<a href="https://store.halfelf.org/donate/">Donate</a>';
+                $donate_link = '<a href="https://ko-fi.com/A236CENl/">Donate</a>';
                 $links[] = $donate_link;
         }
         return $links;
